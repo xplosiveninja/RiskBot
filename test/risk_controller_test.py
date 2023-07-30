83fe4648ea6a402ccf2833d11c7a76dd8b1ce3cf
Charlie Nicholls <charlie.nicholls885@gmail.com>
2023-07-30 21:53:29 +0100

    Added get_region to risk_controller

--- test/risk_controller_test.py
+++ test/risk_controller_test.py
@@ -16,10 +16,15 @@ class game_states:
             board.perform_action(0, value)
         
         board.get_actions()
-        board.perform_action(1, board.game_board.territory_dict["Ural"], 14)
+        board.perform_action(1, board.get_region("Ural"), 14)
         
         board.get_actions()
-        board.perform_action(1, board.game_board.territory_dict["Afghanistan"], 14)
+        board.perform_action(1, board.get_region("Afghanistan"), 14)
+
+        return board
+    
+    def base_setup():
+        board = risk_controller.board(["player_1", "player_2"])
 
         return board
 
@@ -47,18 +52,18 @@ class risk_controller_unit_test(unittest.TestCase):
             board.perform_action(0, value)
         
         board.get_actions()
-        board.perform_action(1, board.game_board.territory_dict["Ural"], 14)
+        board.perform_action(1, board.get_region("Ural"), 14)
         
         board.get_actions()
-        board.perform_action(1, board.game_board.territory_dict["Afghanistan"], 14)
+        board.perform_action(1, board.get_region("Afghanistan"), 14)
         
         board.get_actions()
         self.assertEqual(board.possible_actions, [board.current_player.attack_territory, board.current_player.move_troops])
 
-        board.perform_action(0, 3, board.game_board.territory_dict["Ural"], 2, board.game_board.territory_dict["Afghanistan"])
+        board.perform_action(0, 3, board.get_region("Ural"), 2, board.get_region("Afghanistan"))
         self.assertEqual(board.possible_actions, [board.current_player.attack_territory, board.current_player.move_troops])
 
-        board.perform_action(1, board.game_board.territory_dict["Ural"], board.game_board.territory_dict["Great Britain"], 2)
+        board.perform_action(1, board.get_region("Ural"), board.get_region("Great Britain"), 2)
         board.get_actions()
         self.assertEqual(board.possible_actions, [board.current_player.assign_recruits])
 
@@ -66,30 +71,38 @@ class risk_controller_unit_test(unittest.TestCase):
         board = game_states.assigned_troops_1()
 
         board.get_actions()
-        before = board.game_board.territory_dict["Ural"].troops + board.game_board.territory_dict["Afghanistan"].troops
-        board.perform_action(0, 3, board.game_board.territory_dict["Ural"], 2, board.game_board.territory_dict["Afghanistan"])
-        self.assertTrue(board.game_board.territory_dict["Ural"].troops + board.game_board.territory_dict["Afghanistan"].troops < before)
+        before = board.get_region("Ural").troops + board.get_region("Afghanistan").troops
+        board.perform_action(0, 3, board.get_region("Ural"), 2, board.get_region("Afghanistan"))
+        self.assertTrue(board.get_region("Ural").troops + board.get_region("Afghanistan").troops < before)
 
-        self.assertRaises(Exception, board.perform_action, 0, 5, board.game_board.territory_dict["Ural"], 2, board.game_board.territory_dict["Afghanistan"])
+        self.assertRaises(Exception, board.perform_action, 0, 5, board.get_region("Ural"), 2, board.get_region("Afghanistan"))
 
-        self.assertRaises(Exception, board.perform_action, 0, 1, board.game_board.territory_dict["China"], 2, board.game_board.territory_dict["Afghanistan"])
+        self.assertRaises(Exception, board.perform_action, 0, 1, board.get_region("China"), 2, board.get_region("Afghanistan"))
 
-        self.assertRaises(Exception, board.perform_action, 0, 1, board.game_board.territory_dict["Ural"], 1, board.game_board.territory_dict["Siberia"])
+        self.assertRaises(Exception, board.perform_action, 0, 1, board.get_region("Ural"), 1, board.get_region("Siberia"))
 
-        self.assertRaises(Exception, board.perform_action, 0, 1, board.game_board.territory_dict["Ural"], 2, board.game_board.territory_dict["Ukraine"])
+        self.assertRaises(Exception, board.perform_action, 0, 1, board.get_region("Ural"), 2, board.get_region("Ukraine"))
 
     def test_take_over(self):
         board = game_states.assigned_troops_1()
 
-        prev_owner = board.game_board.territory_dict["Ukraine"].owner.name
+        prev_owner = board.get_region("Ukraine").owner.name
 
         board.get_actions()
         for i in range(self.max_iter):
             try:
-                board.perform_action(0, 3, board.game_board.territory_dict["Ural"], 1, board.game_board.territory_dict["Ukraine"])
+                board.perform_action(0, 3, board.get_region("Ural"), 1, board.get_region("Ukraine"))
             except:
                 pass
 
-        self.assertTrue(board.game_board.territory_dict["Ukraine"].owner.name != prev_owner)
+        self.assertTrue(board.get_region("Ukraine").owner.name != prev_owner)
+
+    def test_get_region(self):
+        board = game_states.base_setup()
+
+        territory = board.get_region("Ukraine")
+
+        self.assertTrue(territory.name == "Ukraine")
+
         
 unittest.main()
